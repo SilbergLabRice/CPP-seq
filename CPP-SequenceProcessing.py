3b57573f2c0b946290b6dbc28abdaa872b4b53c1
jatk <josh.t.atkinson@gmail.com>
2017-11-29 13:07:53 -0600

    Remove extraneous text from Sequence Processing script

--- CPP-SequenceProcessing.py
+++ CPP-SequenceProcessing.py
@@ -1,18 +1,3 @@
-######################
-###CPP-seq Analysis###
-######################
-
-########
-#Inputs:
-########
-    #ParentSequences folder = (1) A FASTA file of variable region (Gene+linker) with the gene name as the ID (e.g. TnAK), and (2) FASTA file of Transposon used for CPP-Seq
-    #MiSeq_Data folder: For each sequence in the variable region FASTA file you need two FASTQ files (1) from unselected library, and (2) from selected library 
-
-#########
-#Outputs:
-#########
-    #(1) Summary text file for all sequences analyzed, for each seqeuence (2) FoldChange and (3)FishersTest and the following for both the selected and unselected libraries of each sequence: (2)Raw Counts, (3) Tagged counts, (4)NuclotidePositionCounts, (5)AminoAcidTerminiCounts 
-
 import re, sys
 import UTIL.UTIL as util
 import Bio.SeqIO
